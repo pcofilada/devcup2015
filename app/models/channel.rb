1854de287a0372ca48d7310018549c9520d7aa6b
Patrick <pcofilada@gmail.com>
2015-08-15 15:33:05 +0800

    Add Channel model validations

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,3 +1,11 @@
 class Channel < ActiveRecord::Base
   belongs_to :owner, polymorphic: true
+
+  validates :title, :owner, presence: true
+  validates :description, presence: true, length: { maximu: 140 }
+  validates :status, presence: true, inclusion: { in: STATUS, message: "%{value} is not a valid status." }
+  validates :category, presence: true, inclusion: { in: CATEGORY, message: "%{value} is not a valid status." }
+
+  STATUS = ['active', 'inactive']
+  CATEGORY = ['private', 'public']
 end
