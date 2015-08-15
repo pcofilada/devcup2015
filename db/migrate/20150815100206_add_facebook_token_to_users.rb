7dfb435b2200a6ab0be414733dcec213b18e4ba3
Patrick <pcofilada@gmail.com>
2015-08-15 18:02:39 +0800

    Add facebook_token field to users

---
+++ db/migrate/20150815100206_add_facebook_token_to_users.rb
@@ -0,0 +1,6 @@
+class AddFacebookTokenToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :facebook_token, :string
+    add_index :users, :facebook_token
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815072347) do
+ActiveRecord::Schema.define(version: 20150815100206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,9 +54,11 @@ ActiveRecord::Schema.define(version: 20150815072347) do
     t.string   "auth_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "facebook_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["facebook_token"], name: "index_users_on_facebook_token", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
