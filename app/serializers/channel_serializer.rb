a67905a7d00be00b6741c0f29a4def13453c3ab5
Patrick <pcofilada@gmail.com>
2015-08-16 01:00:12 +0800

    Add channel serializer

---
+++ app/serializers/channel_serializer.rb
@@ -0,0 +1,11 @@
+class ChannelSerializer < ActiveModel::Serializer
+  attributes :id, :logo, :title, :status, :category, :description, :owner, :announcements, :subscribers
+
+  def logo
+    "#{Rails.application.secrets.host_name}#{object.logo.url(:medium)}"
+  end
+
+  def subscribers
+    "#{object.subscriptions.count}"
+  end
+end
