57ca784cb62a1e574d751ccfe3d84e32cf2d9990
Patrick <pcofilada@gmail.com>
2015-08-16 00:59:58 +0800

    Add logo to channel validations

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -8,7 +8,7 @@ class Channel < ActiveRecord::Base
   has_attached_file :logo, :styles => { :medium => "250x250>", :thumb => "50x50#" }
   validates_attachment_content_type :logo, :content_type => /^image\/(png|jpg|jpeg)/
 
-  validates :title, :owner, presence: true
+  validates :title, :owner, :logo, presence: true
   validates :description, presence: true, length: { maximum: 140 }
   validates :status, presence: true, inclusion: { in: %w(active inactive), message: "%{value} is not a valid status." }
   validates :category, presence: true, inclusion: { in: %w(private public), message: "%{value} is not a valid category." }
