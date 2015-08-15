7e513147e4a216ceca870cf96cdcf10e487368ed
Patrick <pcofilada@gmail.com>
2015-08-15 15:38:20 +0800

    Fixes Channel model validations

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,9 +2,9 @@ class Channel < ActiveRecord::Base
   belongs_to :owner, polymorphic: true
 
   validates :title, :owner, presence: true
-  validates :description, presence: true, length: { maximu: 140 }
-  validates :status, presence: true, inclusion: { in: STATUS, message: "%{value} is not a valid status." }
-  validates :category, presence: true, inclusion: { in: CATEGORY, message: "%{value} is not a valid status." }
+  validates :description, presence: true, length: { maximum: 140 }
+  validates :status, presence: true, inclusion: { in: %w(STATUS), message: "%{value} is not a valid status." }
+  validates :category, presence: true, inclusion: { in: %w(CATEGORY), message: "%{value} is not a valid category." }
 
   STATUS = ['active', 'inactive']
   CATEGORY = ['private', 'public']
