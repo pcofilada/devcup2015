1ac81eae080a9aa00efd9cb4fb7505c81571829a
Patrick <pcofilada@gmail.com>
2015-08-15 16:05:09 +0800

    Add channels_controller create action

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -1,10 +1,26 @@
 class Api::V1::ChannelsController < ApplicationController
-  before_action :authenticate_with_token, only: [:index]
+  before_action :authenticate_with_token, only: [:index, :create]
 
   def index
     channels = current_user.channels
 
     render json: channels, status: 200
+  end
+
+  def create
+    channel = current_user.channels.new(channel_params)
+
+    if channel.save
+      render json: channel, status: 201
+    else
+      render json: { errors: channel.errors.full_messages }, status: 422
+    end
+  end
+
+private
+
+  def channel_params
+    params.require(:channel).permit(:logo, :title, :description, :status, :category)
   end
 
 end
