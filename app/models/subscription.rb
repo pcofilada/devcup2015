fc8acf1baf9a526b69fee2b9b56be7efbda2cc18
Patrick <pcofilada@gmail.com>
2015-08-16 00:50:58 +0800

    Add subscription scope

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,5 +4,10 @@ class Subscription < ActiveRecord::Base
 
   validates :user, :channel, presence: true
   validates :email, :sms, :mobile, inclusion: { in: [true, false] }
+  validates :status, presence: true, inclusion: { in: %w(active inactive), message: "%{value} is not a valid status." }
   validates :user, uniqueness: { scope: :channel, message: 'already subscribed!' }
+
+  scope :sms_enabled, -> { where(sms: true, status: 'active') }
+  scope :email_enabled, -> { where(email: true, status: 'active') }
+  scope :mobile_enabled, -> { where(mobile: true, status: 'active') }
 end
