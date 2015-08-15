386c0ca2d835abe62bca4f873d290a075ce22d8e
Patrick <pcofilada@gmail.com>
2015-08-16 00:15:44 +0800

    Add subscriptions_controller

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::SubscriptionsController < ApplicationController
+  before_action :authenticate_with_token
+  before_action :set_channel, only: :create
+
+  def create
+    subscription = Subscription.new(subscription_params)
+    subscription.user = current_user
+    subscriotion.channel = @channel
+
+    if subscription.save
+      render json: subscription, status: 201
+    else
+      render json: { errors: subscription.errors.full_messages }, status: 422
+    end
+  end
+
+private
+
+  def set_channel
+    @channel = Channel.find(params[:channel_id])
+  end
+
+  def subscription_params
+    params.require(:subscription).permit(:email, :sms, :mobile)
+  end
+end
