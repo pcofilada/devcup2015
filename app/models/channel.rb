267f9d4e8618b1444446c87836064f6daf907fd4
Patrick <pcofilada@gmail.com>
2015-08-15 15:54:22 +0800

    Add logo on Channel model

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,6 +1,9 @@
 class Channel < ActiveRecord::Base
   belongs_to :owner, polymorphic: true
 
+  has_attached_file :logo, :styles => { :medium => "250x250>", :thumb => "50x50#" }
+  validates_attachment_content_type :logo, :content_type => /^image\/(png|jpg|jpeg)/
+
   validates :title, :owner, presence: true
   validates :description, presence: true, length: { maximum: 140 }
   validates :status, presence: true, inclusion: { in: %w(active inactive), message: "%{value} is not a valid status." }
