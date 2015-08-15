f1f3cdc113425573cc2be14b2d57a610d27767a5
Patrick <pcofilada@gmail.com>
2015-08-15 22:25:38 +0800

    Add announcements_controller create path

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: :create do
-        resources :channels, only: [:index, :create, :update]
+        resources :channels, only: [:index, :create, :update] do
+          resources :announcements, only: [:create]
+        end
       end
       resources :sessions, only: [:create, :destroy]
       post 'facebook', to: 'sessions#facebook_login'
